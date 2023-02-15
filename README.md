e1a1f34055a1683b0574a8beddcd5dd9b29f2421
p0p3yee <devp0p3yee@gmail.com>
2023-02-15 15:06:09 -0500

    Fix using incorrect ID & Add decryption test

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ An executable for encrypting plain text using DistributedIBE encryption with giv
 
 ## Example:
 
+### To Encrypt a plaintext
+
 `./encrypter Random_IBE_ID 84dbb7681181e69db71a99e9427344a4478a8b2911f3c6ef36a1891b2e6b4fcfee5c6942ca42502eafe6fc7ec782f60d "Hello World"`
+
+
+### To Encrypt a plaintext & Decrypt it
+
+`./encrypter Random_IBE_ID 84dbb7681181e69db71a99e9427344a4478a8b2911f3c6ef36a1891b2e6b4fcfee5c6942ca42502eafe6fc7ec782f60d "Hello World" b45ee7403c8b3f4dfbdbada34a7c060a818b97ba66865663967f3ba912d4438430b43aacb5db1ea62168bac6171d148d0f6bb3389321dc69bf1420ce03cbceb3e4cff764252f9b1dd476f09f4ff958b6d06a149aec3d5c567afd1f05a1417dc8`
--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 const EXPECTING_ARG_NUM = 4
 
 func main() {
-	if len(os.Args) != EXPECTING_ARG_NUM {
-		panic(fmt.Sprintf("\nExpecting %d arguments, got %d arguments. Usage: ./encrypter <ID> <publickey> <plaintext>\n", EXPECTING_ARG_NUM, len(os.Args)))
+	if len(os.Args) < EXPECTING_ARG_NUM {
+		panic(fmt.Sprintf("\nExpecting %d arguments, got %d arguments. Usage: ./encrypter <ID> <publickey> <plaintext> <optional: privatekey>\n", EXPECTING_ARG_NUM, len(os.Args)))
 	}
 
 	suite := bls.NewBLS12381Suite()
@@ -30,10 +30,50 @@ func main() {
 	}
 
 	var destCipherData bytes.Buffer
-	err = enc.Encrypt(publicKeyPoint, []byte(os.Args[2]), &destCipherData, bytes.NewBuffer([]byte(os.Args[3])))
+	var plainTextBuffer bytes.Buffer
+	_, err = plainTextBuffer.WriteString(os.Args[3])
+	if err != nil {
+		panic(fmt.Sprintf("\nError writing plaintext string to buffer: %s\n", err.Error()))
+	}
+
+	err = enc.Encrypt(publicKeyPoint, []byte(os.Args[1]), &destCipherData, &plainTextBuffer)
 	if err != nil {
 		panic(fmt.Sprintf("\nError encrypting: %s\n", err.Error()))
 	}
 
-	fmt.Println(hex.EncodeToString(destCipherData.Bytes()))
+	hexCipher := hex.EncodeToString(destCipherData.Bytes())
+
+	fmt.Println(hexCipher)
+
+	if len(os.Args) == EXPECTING_ARG_NUM+1 {
+		privateKeyByte, err := hex.DecodeString(os.Args[4])
+		if err != nil {
+			panic(fmt.Sprintf("\nError decoding private key: %s\n", err.Error()))
+		}
+
+		privateKeyPoint := suite.G2().Point()
+		err = privateKeyPoint.UnmarshalBinary(privateKeyByte)
+		if err != nil {
+			panic(fmt.Sprintf("\nError unmarshalling private key: %s\n", err.Error()))
+		}
+
+		cipherBytes, err := hex.DecodeString(hexCipher)
+		if err != nil {
+			panic(fmt.Sprintf("\nError decoding cipher from hex to bytes: %s\n", err.Error()))
+		}
+
+		var destPlainText bytes.Buffer
+		var cipherBuffer bytes.Buffer
+		_, err = cipherBuffer.Write(cipherBytes)
+		if err != nil {
+			panic(fmt.Sprintf("\nError writing plaintext string to buffer: %s\n", err.Error()))
+		}
+
+		err = enc.Decrypt(publicKeyPoint, privateKeyPoint, &destPlainText, &cipherBuffer)
+		if err != nil {
+			panic(fmt.Sprintf("\nError decrypting: %s\n", err.Error()))
+		}
+
+		fmt.Printf("\nDecrypt Cipher Successfully:\n%s\n", destPlainText.String())
+	}
 }
